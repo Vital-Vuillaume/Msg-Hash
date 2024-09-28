7bffa9636c116f82dcae2b81442664989cfa52e7
vuillaume <vitalvuillaume@proton.me>
2024-09-28 23:44:51 +0200

    test

---
+++ msg-hash.js
@@ -0,0 +1,39 @@
+//------Https------\\
+
+/*if (window.location.protocol != "https:") {
+  window.location.protocol="https:";
+}*/
+
+const hash = document.querySelector(".hash");
+const txt = document.querySelector(".txt");
+const txtHash = document.querySelector(".txtHash");
+
+let hashValue
+
+hash.addEventListener("input", function() {
+  hashValue = hash.value;
+})
+
+function encryptDecrypt(text, hashValue) {
+  let result = '';
+  for (let i = 0; i < text.length; i++) {
+    let textChar = text.charCodeAt(i);
+    let hashChar = hashValue.charCodeAt(i % hashValue.length);
+    result += String.fromCharCode(textChar ^ hashChar);
+  }
+  return result;
+}
+
+txt.addEventListener("input", function() {
+  try {
+    let encrypted = encryptDecrypt(txt.value, hashValue);
+    txtHash.value = btoa(encrypted);
+  } catch (error) {}
+});
+
+txtHash.addEventListener("input", function() {
+  try {
+    let decrypted = atob(txtHash.value);
+    txt.value = encryptDecrypt(decrypted, hashValue);
+  } catch (error) {}
+});
